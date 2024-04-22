1f77e5b1b88afe4c17a01b4fa76ea8a5509c0170
Zeanderson <121685062+Zeanderson@users.noreply.github.com>
2024-04-21 23:26:34 -0500

    Basic route implementation Show players works

--- client/src/routes/functions/showPlayers.tsx
+++ client/src/routes/functions/showPlayers.tsx
@@ -1,40 +1,123 @@
 import HomeLink from "../../components/HomeLink";
 import Header from "../../components/Header";
 
-function PlayerTable() {
-  // Array of players passed as props, to be displayed in a table
+import { useState } from "react";
+import axios from "axios";
+import { useQuery } from "react-query";
+
+function getTeams() {
+  return useQuery("teams", async () => {
+    const response = await axios.get("http://127.0.0.1:5000/getTeams");
+    return response.data;
+  });
+}
+
+function getPlayers(teamId: number) {
+  return useQuery("players", async () => {
+    const response = await axios.get(
+      `http://127.0.0.1:5000/getPlayers/${teamId}`
+    );
+    return response.data;
+  });
+}
+
+type Player = {
+  TeamID: number;
+  PlayerID: number;
+  Name: string;
+  Position: string;
+};
+
+type PlayerTableProps = {
+  teamID: number;
+};
+
+function PlayerTable({ teamID }: PlayerTableProps) {
+  const response = getPlayers(teamID);
+
+  if (response.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  const players = response.data;
+
   return (
-    <table>
-      <tr>
-        <th>Player ID</th>
-        <th>Team ID</th>
-        <th>Name</th>
-        <th>Position</th>
-      </tr>
-      <tr>
-        <td>1</td>
-        <td>John</td>
-        <td>Doe</td>
-        <td>Team 1</td>
-      </tr>
-      <tr>
-        <td>2</td>
-        <td>Jane</td>
-        <td>Doe</td>
-        <td>Team 2</td>
-      </tr>
-    </table>
+    <div className="grid grid-cols-3 gap-2">
+      {players.map((player: Player) => {
+        return (
+          <div
+            key={player.PlayerID}
+            // className="justify-center bg-Corp3 rounded-xl p-2 items-center transition-colors min-w-[10rem]"
+            className="flex flex-col items-center bg-Nature2 rounded-xl p-2"
+          >
+            <p>{player.Name}</p>
+            <p>{player.Position}</p>
+          </div>
+        );
+      })}
+    </div>
   );
 }
 
-function ShowPlayers() {
-  //TODO API call to get all teams
-  // const [players, setPlayers] = useState([]);
-  // When selecting a team, set the players state from the api call and pass into playertable to display
+type Team = {
+  TeamID: number;
+  Location: string;
+  Division: string;
+  Conf: string;
+  Nickname: string;
+};
+
+type TeamTableProps = {
+  teams: Team[];
+};
+
+function TeamTable({ teams }: TeamTableProps) {
+  const [selectedID, setSelectedID] = useState(0);
+
   return (
     <div className="flex flex-col gap-2 items-center">
+      <p>Select a team you would like to see players from!!!</p>
+      <div className="grid grid-cols-3 gap-4">
+        {teams.map((team: Team) => {
+          let selectedColor = "";
+          if (selectedID === team.TeamID) {
+            selectedColor = "bg-Nature2";
+          }
+          return (
+            <button
+              key={team.TeamID}
+              onClick={() => {
+                setSelectedID(team.TeamID);
+              }}
+              className={
+                "justify-center w-full max-w-[15rem] bg-Corp3 rounded-xl p-4 items-center transition-colors hover:bg-Corp4 focus:outline-none " +
+                selectedColor
+              }
+            >
+              {team.Nickname}
+            </button>
+          );
+        })}
+      </div>
+      {selectedID !== 0 && <PlayerTable teamID={selectedID} />}
+    </div>
+  );
+}
+
+//TODO Find a way to refetch on button click, so it does not take FOREVER TO LOAD
+
+function ShowPlayers() {
+  const response = getTeams();
+
+  if (response.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  const teams = response.data;
+  return (
+    <div className="flex flex-col gap-5 items-center">
       <Header name="Show Players" />
-      <PlayerTable />
+      <TeamTable teams={teams} />
       <HomeLink />
     </div>
   );
--- flask-server/app.py
+++ flask-server/app.py
@@ -7,7 +7,7 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-# CORS(app, origins="http://localhost:5173")
+CORS(app, origins="http://localhost:5173")
 
 def get_db_connection():
     try:
@@ -26,12 +26,15 @@ def get_db_connection():
 
 
 @app.route('/')
-# @cross_origin()
+@cross_origin()
 def index():
     return {"home": ["homevar", "homevar2", "homevar3"]}
 
-@app.route('/query')
-def execute_query():
+#!! DONE !!#
+# This gets all teams from the database
+@app.route('/getTeams')
+@cross_origin()
+def getTeams():
     try:
         # Establish a connection to the database
         connection = get_db_connection()
@@ -41,8 +44,8 @@ def execute_query():
         # Create a cursor object
         cursor = connection.cursor()
 
-        # Execute a sample query
-        cursor.execute("SELECT * FROM player LIMIT 5")
+        # Execute a query to select players from the specified team
+        cursor.execute("SELECT * FROM team")
 
         # Fetch all the rows
         rows = cursor.fetchall()
@@ -52,7 +55,38 @@ def execute_query():
         connection.close()
 
         # Convert the results to JSON
-        results = [{"id": row[0], "name": row[1]} for row in rows]
+        results = [{"TeamID": row[0], "Location": row[1], "Nickname": row[2], "Conf": row[3], "Division": row[4]} for row in rows]
+        return jsonify(results)
+    
+    except psycopg2.Error as e:
+        return jsonify({"error": str(e)}), 500
+
+#!! DONE !!#
+# This takes a teamId and returns all the players in that team
+@app.route('/getPlayers/<string:teamId>')
+@cross_origin()
+def getPlayers(teamId):
+    try:
+        # Establish a connection to the database
+        connection = get_db_connection()
+        if connection is None:
+            return jsonify({"error": "Failed to connect to the database."}), 500
+        
+        # Create a cursor object
+        cursor = connection.cursor()
+
+        # Execute a query to select players from the specified team
+        cursor.execute("SELECT * FROM player WHERE teamid = %s", (teamId))
+
+        # Fetch all the rows
+        rows = cursor.fetchall()
+
+        # Close the cursor and connection
+        cursor.close()
+        connection.close()
+
+        # Convert the results to JSON
+        results = [{"PlayerID": row[0], "TeamID": row[1], "Name": row[2], "Position": row[3]} for row in rows]
         return jsonify(results)
     
     except psycopg2.Error as e:
